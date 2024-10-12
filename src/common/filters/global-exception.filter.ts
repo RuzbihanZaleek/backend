b0f1520d1f7e5a40dad820bb86fca5685728ce34
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-13 00:29:45 +0530

    created global exception filter

---
+++ src/common/filters/global-exception.filter.ts
@@ -0,0 +1,27 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { Request, Response } from "express";
+import {MESSAGES} from '../constants/messages.constants';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+    catch(exception: any, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const request = ctx.getRequest<Request>();
+        const response = ctx.getResponse<Response>();
+        const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        const responseBody = {
+            statusCode: status,
+            timestamp: new Date().toISOString(),
+            path: request.url,
+            message: exception.message || MESSAGES.ERROR.INTERNAL_SERVER_ERROR,
+            validationErrors: ''
+        };
+
+        if (exception.status === HttpStatus.BAD_REQUEST && exception.validationErrors) {
+            responseBody.validationErrors = exception.validationErrors;
+        }
+
+        response.status(status).json(responseBody);
+    }
+}
