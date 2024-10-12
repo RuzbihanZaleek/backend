f1ca9ec23572d844273c602bb2514ca6d3124aae
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-13 00:30:19 +0530

    created messages constant file

---
+++ src/common/constants/messages.constants.ts
@@ -0,0 +1,15 @@
+
+export const MESSAGES = {
+    SUCCESS: {
+      USER_CREATED: 'User has been successfully created.',
+      USER_UPDATED: 'User has been successfully updated.',
+      USER_DELETED: 'User has been successfully deleted.',
+    },
+    ERROR: {
+      USER_NOT_FOUND: 'User not found.',
+      EMAIL_ALREADY_EXISTS: 'This email already exists.',
+      UNAUTHORIZED: 'You are not authorized to perform this action.',
+      INTERNAL_SERVER_ERROR: 'Internal server error',
+      VALIDATION_FAILED: 'Validation failed',
+    },
+  };
